1337dbc49dc5232d77b17b44d12c0e3874e80947
oracleofnj <jared.samet@aya.yale.edu>
2019-04-12 16:57:05 -0400

    Add implicit

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,22 @@ pub mod einsum {
     use lazy_static::lazy_static;
 
     #[derive(Debug)]
-    pub struct EinsumExpression {
+    pub struct EinsumParse {
         num_operands: usize,
         operand_indices: Vec<String>,
-        output_indices: String
+        output_indices: Option<String>
     }
 
-    pub fn parse_einsum_string(input_string: &str) -> Option<EinsumExpression> {
-        // Einsum string has the form:
-        //
-        // ([a..z]*,)*([a-z]*)->([a-z]*)
+    pub fn parse_einsum_string(input_string: &str) -> Option<EinsumParse> {
         lazy_static! {
+            // Unwhitespaced version:
+            // ^([a-z]+)((?:,[a-z]+)*)(?:->([a-z]*))?$
+
             static ref RE: Regex = Regex::new(r"(?x)
             ^
             (?P<first_operand>[a-z]+)
             (?P<more_operands>(?:,[a-z]+)*)
-            ->
-            (?P<output>[a-z]*)
+            (?:->(?P<output>[a-z]*))?
             $
             ").unwrap();
         }
@@ -27,6 +26,9 @@ pub mod einsum {
         let captures = RE.captures(input_string)?;
         let mut num_operands = 1;
         let mut operand_indices = Vec::new();
+        let output_indices = captures.name("output").map(|s|  {
+            String::from(s.as_str())
+        });
 
         operand_indices.push(String::from(&captures["first_operand"]));
         for s in (&captures["more_operands"]).split(',').skip(1) {
@@ -34,16 +36,17 @@ pub mod einsum {
             num_operands += 1;
         }
 
-        Some(EinsumExpression {
+        Some(EinsumParse {
             num_operands: num_operands,
             operand_indices: operand_indices,
-            output_indices: String::from(&captures["output"])
+            output_indices: output_indices
         })
     }
 }
 
 fn main() {
     for test_string in [
+        // Explicit
         "i->",
         "ij->",
         "i->i",
@@ -53,10 +56,18 @@ fn main() {
         "ij,jk->ik",
         "i,j,k,l,m->p",
         "ijk,jkl,klm->im",
+
+        // Implicit
+        "ij,k",
+        "i",
+        "iii",
+        "i,j,klm,nop",
+
+        // Illegal
         "->i",
         "i,",
-        "ij,k",
         "->",
+        "i,,,j->k"
     ].iter() {
         println!("{}", test_string);
         match einsum::parse_einsum_string(test_string) {
